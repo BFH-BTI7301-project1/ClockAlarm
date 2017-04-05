22262e97998389ae001e3db5fcbaa2206540f9f9
Samuel Gauthier <samuel.luc.gauthier@gmail.com>
2017-04-05 15:29:29 +0200

    [#44] finish the Notification test and closes #44

--- _clockalarm/_tests/Notification_test.py
+++ _clockalarm/_tests/Notification_test.py
@@ -1,7 +1,12 @@
 from _clockalarm import Notification
+from PyQt5.QtGui import QColor, QFont
+from PyQt5.QtMultimedia import QSound
 
 def test_notification():
     notification = Notification("Test")
     
+    assert isinstance(notification.color, QColor)
+    assert isinstance(notification.font, QFont)
+    assert isinstance(notification.sound, QSound)
     assert notification.message == "Test"
 
