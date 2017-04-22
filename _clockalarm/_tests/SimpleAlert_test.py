febe0f6ffba61435e2886901333ee20e0465bf91
Samuel Gauthier <samuel.luc.gauthier@gmail.com>
2017-04-22 22:09:03 +0200

    [#45] add SimpleAlert tests

---
+++ _clockalarm/_tests/SimpleAlert_test.py
@@ -0,0 +1,20 @@
+from _clockalarm import SimpleAlert
+from _clockalarm import Alert
+from _clockalarm import Notification
+
+def test_simple_alert_constructor():
+    """Test the :class:`~_clockalarm.SimpleAlert` constructor."""
+    simplealert = SimpleAlert(10, "Test")
+    
+    assert simplealert.trigger_time == 10
+    assert simplealert._notification.message == "Test"
+
+def test_simple_alert_triggered():
+    """Test the :class:`~_clockalarm.SimpleAlert.triggered` method."""
+    pass
+
+def test_get_identifier():
+    """Test the :class:`~_clockalarm.SimpleAlert.get_identifier` method."""
+    simplealert = SimpleAlert(10, "Test")
+    
+    assert simplealert.get_identifier() == "Test"
