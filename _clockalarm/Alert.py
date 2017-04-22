caf780307a85a0c9f7e4b01b9af84e2d7d2eab88
Samuel Gauthier <samuel.luc.gauthier@gmail.com>
2017-04-22 22:02:17 +0200

    comment Alert and SimpleAlert

--- _clockalarm/Alert.py
+++ _clockalarm/Alert.py
@@ -5,24 +5,37 @@ from PyQt5.QtCore import pyqtSignal, QObject
 
 
 class Alert(QObject):
+    """Abstract Alert class representing alerts.
+    
+    Attributes:
+        trigger_time: The trigger time of the alert.
+    """
     __metaclass__ = abc.ABCMeta
 
     timeout = pyqtSignal('PyQt_PyObject')
 
     def __init__(self, trigger_time):
+        """Inits Alert class with the trigger time given in arguments."""
         super(Alert, self).__init__()
-        """Constructor"""
         self.trigger_time = trigger_time
 
     def kill(self):
+        """Kills the Alert.
+
+        Removes the parent of the alert.
+        """
         self.setParent(None)
 
     @abstractmethod
     def triggered(self):
-        """Triggers action"""
+        """Triggers the alert action."""
         return
 
     @abstractmethod
     def get_identifier(self):
-        """Return an identifier"""
+        """Get the alert identifier.
+        
+        Returns:
+            The identifier of the alert.
+        """
         return str
--- _clockalarm/SimpleAlert.py
+++ _clockalarm/SimpleAlert.py
@@ -3,12 +3,26 @@ from _clockalarm import Notification
 
 
 class SimpleAlert(Alert):
+    """Simple Alert implementation with text message
+
+    Attributes:
+        trigger_time: The time at which the alert is triggered.
+        message: The text message to display when the alert is triggered.
+    """
+
     def __init__(self, trigger_time, message):
+        """Default constructor for the SimpleAlert class."""
         super(SimpleAlert, self).__init__(trigger_time)
         self._notification = Notification(message)
 
     def triggered(self):
+        """This method does the same as
+        :func:`~_clockalarm.Alert.triggered`
+        """
         self.timeout.emit(self._notification)
 
     def get_identifier(self):
+        """This method does the same as
+        :func:`~_clockalarm.Alert.get_identifier`
+        """
         return self._notification.message
