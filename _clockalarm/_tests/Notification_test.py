0d5cbd9e7756831885411e3bd96e5c3c0cf91268
Samuel Gauthier <samuel.luc.gauthier@gmail.com>
2017-05-20 17:02:42 +0200

    update tests

--- _clockalarm/_tests/Notification_test.py
+++ _clockalarm/_tests/Notification_test.py
@@ -1,5 +1,5 @@
 from PyQt5.QtGui import QColor, QFont
-from PyQt5.QtMultimedia import QSound
+from pygame import mixer
 
 from _clockalarm import Notification
 
@@ -8,7 +8,42 @@ def test_notification():
     """Tests the :class:`~_clockalarm.Notification` constructor."""
     notification = Notification("Test")
 
-    assert isinstance(notification.color, QColor)
-    assert isinstance(notification.font, QFont)
-    assert isinstance(notification.sound, QSound)
     assert notification.message == "Test"
+    assert not notification.color_hex
+    assert not notification.font_family
+    assert not notification.font_size
+    assert not notification.sound
+
+
+def test_get_message():
+    """Tests the :class:`~_clockalarm.Notification.get_message` method."""
+    notification = Notification("Test")
+
+    assert notification.get_message() == "Test"
+
+
+def test_get_font():
+    """Tests the :class:`~_clockalarm.Notification.get_font` method without any
+    font given.
+    """
+    notification = Notification("Test")
+
+    assert isinstance(notification.get_font(), QFont)
+
+
+def test_get_color():
+    """Tests the :class:`~_clockalarm.Notification.get_color` method without any
+    color given.
+    """
+    notification = Notification("Test")
+
+    assert isinstance(notification.get_color(), QColor)
+
+
+def test_get_sound():
+    """Tests the :class:`~_clockalarm.Notification.get_sound` method without any
+    sound given.
+    """
+    notification = Notification("Test")
+
+    assert isinstance(notification.get_sound(), mixer.Sound)
--- _clockalarm/_tests/SimpleAlert_test.py
+++ _clockalarm/_tests/SimpleAlert_test.py
@@ -1,12 +1,15 @@
-from _clockalarm import SimpleAlert
+from _clockalarm.SimpleAlert import SimpleAlert
+from _clockalarm import Notification
 
 
 def test_simple_alert_constructor():
     """Test the :class:`~_clockalarm.SimpleAlert` constructor."""
-    simplealert = SimpleAlert(10, "Test")
+    notification = Notification("Test")
+    simplealert = SimpleAlert(10, notification)
 
     assert simplealert.trigger_time == 10
-    assert simplealert._notification.message == "Test"
+    assert simplealert.notification.get_message() == "Test"
+    assert not simplealert.periodicity
 
 
 def test_simple_alert_triggered():
@@ -16,6 +19,4 @@ def test_simple_alert_triggered():
 
 def test_get_identifier():
     """Test the :class:`~_clockalarm.SimpleAlert.get_identifier` method."""
-    simplealert = SimpleAlert(10, "Test")
-
-    assert simplealert.get_identifier() == "Test"
+    pass
