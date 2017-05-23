9ceb5f1e726d0285e2642e7d8d1987ea80f4aee5
Samuel Gauthier <samuel.luc.gauthier@gmail.com>
2017-05-23 11:33:54 +0200

    [#45] update SimpleAlert tests

--- _clockalarm/_tests/SimpleAlert_test.py
+++ _clockalarm/_tests/SimpleAlert_test.py
@@ -2,21 +2,35 @@ from _clockalarm.SimpleAlert import SimpleAlert
 from _clockalarm import Notification
 
 
+notification = Notification("Test")
+
+
 def test_simple_alert_constructor():
     """Test the :class:`~_clockalarm.SimpleAlert` constructor."""
-    notification = Notification("Test")
+    global notification
     simplealert = SimpleAlert(10, notification)
 
     assert simplealert.trigger_time == 10
-    assert simplealert.notification.get_message() == "Test"
     assert not simplealert.periodicity
+
+
+def test_get_periodicity():
+    """Test the :class:`~_clockalarm.SimpleAlert.get_periodicity` method."""
+    global notification
+    simplealert = SimpleAlert(10, notification, 30)
+
+    assert simplealert.get_periodicity() == 30
+
+
+def test_get_notification():
+    """Test the :class:`~_clockalarm.SimpleAlert.get_notification` method."""
+    global notification
+    simplealert = SimpleAlert(10, notification)
+
+    assert (simplealert.get_notification().get_message() ==
+            notification.get_message())
 
 
 def test_simple_alert_triggered():
     """Test the :class:`~_clockalarm.SimpleAlert.triggered` method."""
-    pass
-
-
-def test_get_identifier():
-    """Test the :class:`~_clockalarm.SimpleAlert.get_identifier` method."""
     pass
